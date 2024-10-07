9e40e9653f38611729d899d4368388fa2a3e4a95
TRayyyyyyy <vanthuong0917@gmail.com>
2024-10-07 22:46:53 +0700

    update

---
+++ src/main/java/JpaST2/controller/DownloadFileController.java
@@ -0,0 +1,29 @@
+package JpaST2.controller;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import JpaST2.utils.Constant;
+
+import org.apache.commons.io.IOUtils;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@SuppressWarnings("serial")
+@WebServlet(urlPatterns = "/image")
+public class DownloadFileController extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String fileName = req.getParameter("fname");
+		File file = new File(Constant.UPLOAD_DIRECTORY + "/" + fileName);
+		resp.setContentType("image/jpeg");
+		if (file.exists()) {
+			IOUtils.copy(new FileInputStream(file), resp.getOutputStream());
+		}
+	}
+}
+  
